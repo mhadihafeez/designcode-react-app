799949c1cc1b1f107bf01b6ce74e64a6fe7b9d53
Hadi <hadi@maynard.local>
2018-08-03 18:38:26 +0500

    design code chapter 9 complete

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'Design+Code 3',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'a9yo9ouit27e',
+        accessToken: '1cfdebd9c1245a31be53a8e9014c2a872171458da592917bfc8e27c68ee40716'
+      }
+    }
+  ],
 }
